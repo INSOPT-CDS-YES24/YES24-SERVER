83407a420d7a5e394b4bf4be6e60d7cff709c78a
Minjae Kang <rkdalswo1021@naver.com>
2022-11-22 10:49:21 +0900

    [CHORE] 예외처리 message 수정

--- src/controller/ticketController.ts
+++ src/controller/ticketController.ts
@@ -1,16 +1,19 @@
-import { Request, Response } from "express";
-import { ticketService } from "../service";
+import { Request, Response } from 'express';
+import { ticketService } from '../service';
 
 //* 마이티켓 정보 조회
-const getAllTicket = async ( req: Request, res: Response ) => {
+const getAllTicket = async (req: Request, res: Response) => {
   const { userId } = req.params;
 
-  const data = await ticketService.getAllTicket(+userId);
-
-  if (!data) {
-    return res.status(404).json({ status: 404, message: "NOT_FOUND" });
+  if (!userId) {
+    return res
+      .status(400)
+      .json({ status: 400, message: '마이티켓 정보 조회 실패' });
   }
-  return res.status(200).json({ status: 200, message: "마이티켓 정보 조회 성공", data });
+  const data = await ticketService.getAllTicket(+userId);
+  return res
+    .status(200)
+    .json({ status: 200, message: '마이티켓 정보 조회 성공', data });
 };
 
 const ticketController = { getAllTicket };
